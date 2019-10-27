946805dad8792b31876c64250fdc7f86e4cae7c5
MXDA <47274057+MXDA@users.noreply.github.com>
2019-10-27 19:53:48 +0800

    Create LeetCode_144_739.cpp

---
+++ Week 02/id_739/LeetCode_144_739.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> res;
+        stack<TreeNode*> visit;
+        TreeNode* cur = root;
+        while(cur || !visit.empty()){
+            if(cur){
+                visit.push(cur);
+                res.push_back(cur->val);
+                cur = cur->left;
+            }  else{
+                cur = visit.top();
+                visit.pop();
+                cur = cur->right;
+               
+            }
+        }
+        return res;
+    }
+};                 //非递归
+class Solution {
+public:
+    vector<int> res;
+    vector<int> preorderTraversal(TreeNode* root) {
+        if(root){
+            res.push_back(root->val);
+        preorderTraversal(root->left);
+        preorderTraversal(root->right);
+        }
+        return res;
+    }
+};                               //递归 
